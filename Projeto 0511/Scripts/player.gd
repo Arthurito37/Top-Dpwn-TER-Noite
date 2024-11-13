d5c88bd98cd65a6f8c5235031ea54760f5098596
Arthur de Sousa Santana <128152061+Arthurito37@users.noreply.github.com>
2024-11-12 21:20:31 -0300

    Add files via upload

--- Projeto 0511/Scripts/player.gd
+++ Projeto 0511/Scripts/player.gd
@@ -7,39 +7,37 @@ var spd = 50
 
 func _ready():
 	pass
-	
 
-func _physics_process(delta):	
+
+func _physics_process(delta):
 	
 	Move(delta)
 	Anim()
+	
 	pass
-	
 
-func Move(delta): 
-	
-	
-	dir = Input.get_vector("Left", "Right", "Up", "Down")
 
+func Move(delta):
+	
+	dir = Input.get_vector("Left" , "Right" , "Up" , "Down")
+	
 	velocity = dir.normalized() * spd
 	
 	move_and_slide()
 	
 	pass
 
+
 func Anim():
 	
 	if velocity.length() > 0:
-		
-		anim.play("walk")
+		anim.play("Walk")
 	else:
 		anim.play("Idle")
-		
+	
 	if Input.is_action_pressed("Left"):
 		anim.flip_h = true
 	if Input.is_action_pressed("Right"):
 		anim.flip_h = false
 	
-	
 	pass
-	
